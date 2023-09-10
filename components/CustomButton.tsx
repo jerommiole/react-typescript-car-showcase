0681cf2d15e64b5aa9220892eb653661d767e7ac
Jerom Miole <miolejerom@gmail.com>
2023-09-10 17:15:36 +1200

    feat: right icon in the button

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,7 +4,7 @@ import { CustomButtonProps } from '@/types';
 
 import Image from 'next/image'
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyle, rightIcon}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
@@ -12,9 +12,19 @@ const CustomButton = ({ title, containerStyles, handleClick, btnType}: CustomBut
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-        <span className={`flex-1`}>
+        <span className={`flex-1 ${textStyle}`}>
             {title}
         </span>
+        {rightIcon && (
+          <div className='relative w-6 h-6'>
+            <Image
+              src={rightIcon}
+              alt='right icon'
+              fill
+              className='object-contain'
+            />
+          </div>
+        )}
     </button>
   )
 }
