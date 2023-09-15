123979e65fef08f100bf0eb2867e59849f263379
Jerom Miole <miolejerom@gmail.com>
2023-09-15 18:59:32 +1200

    refactor

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,7 +3,7 @@
 import { useState, Fragment } from "react"
 import Image from 'next/image'
 import { Combobox, Transition } from '@headlessui/react'
-import { manufacturers } from "@/contants";
+import { manufacturers } from "@/constants";
 import { SearchManufacturerProps } from '@/types'
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps ) => {
